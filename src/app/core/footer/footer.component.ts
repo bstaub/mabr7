b53a389b542326fbd8029daacdb6dabb46c35678
Bruno Staub <bruno.staub@gmail.com>
2018-11-12 20:37:09 +0100

    playing around with angular template io

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./styles/footer.styles.scss']
 })
 export class FooterComponent implements OnInit {
 
