e8c47862d0663e78ffe6bae37954cb42eaaeacf7
Martin Polak <martinpolak.mail@gmail.com>
2018-11-19 21:52:48 +0100

    bugfix refresh order-preview after reload (on all sites)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderService } from './order/shared/order.service';
+import { LocalStorageService } from './shared/local-storage.service';
+import { AuthService } from './user/shared/auth.service';
+import { OrderFlyoutService } from './core/shared/order-flyout.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./styles/app.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mabr7';
   OffCanvasClickedCheck = false;
+  authSubscription: Subscription;
+  user: any;
+  order: any;
+
+  constructor(
+    private orderFlyoutService: OrderFlyoutService,
+    private localStorageService: LocalStorageService,
+    private authService: AuthService,
+    private orderService: OrderService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.user$.subscribe((user) => {
+      if (user && user.emailVerified) {
+        this.user = user;
+        this.getProducts(user.id);
+      } else {
+        this.getProducts(this.localStorageService.getData('anonymusOrderId').orderId);
+      }
+    });
+  }
+
 
   OffCanvasClicked() {
     this.OffCanvasClickedCheck = true;
@@ -15,5 +43,13 @@ export class AppComponent {
 
   CloseNavState() {
     this.OffCanvasClickedCheck = false;
+  }
+
+  getProducts(orderId) {
+    this.orderService.getUserOrder(orderId).subscribe((res) => {
+      this.order = res;
+      this.orderFlyoutService.refreshOrderFlyout(this.localStorageService.getData('products'), this.order);
+    });
+
   }
 }
