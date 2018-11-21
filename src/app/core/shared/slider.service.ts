2c5fd16b04d000d599f2af619bcb7e7c7ee9ba3e
Martin Polak <mpolak@hsr.ch>
2018-11-21 18:18:54 +0100

    pictures optimized

--- src/app/core/shared/slider.service.ts
+++ src/app/core/shared/slider.service.ts
@@ -25,13 +25,13 @@ export class SliderService {
         link: 'http://localhost:4200/produkte/detail/dMCPQ1NYxKGCa5yvvaII',
       },
       {
-        image: `https://picsum.photos/2560/500?image=64`,
+        image: `assets/slider/slide_sonnenbrillen.jpg`,
         title: 'Sonnenbrillen',
         text: 'Die neusten Brillenmodelle passend zu den Jahrezeiten',
         link: 'http://localhost:4200/produkte/detail/eVR3Tj9bRLIAsjD9CrQ6',
       },
       {
-        image: `https://picsum.photos/2560/500?image=2`,  // https://picsum.photos/2560/500?random&t=${Math.random()}
+        image: `assets/slider/slide_computer.jpg`,  // https://picsum.photos/2560/500?random&t=${Math.random()}
         title: 'Apple Macbook Air',
         text: 'Apple Lifestyle Produkte',
         link: 'http://localhost:4200/produkte/detail/eVR3Tj9bRLIAsjD9CrQ6',
