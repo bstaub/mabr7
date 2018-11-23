9c3e81ffb4e9a62b79e32fb3ad778600f877463f
Bruno Staub <bruno.staub@gmail.com>
2018-11-23 18:30:17 +0100

    admin area styling bootstrap

--- src/app/admin/admin.routing.ts
+++ src/app/admin/admin.routing.ts
@@ -4,6 +4,10 @@ import { AdminProductAddComponent } from './admin-product-add/admin-product-add.
 import { AdminProductListComponent } from './admin-product-list/admin-product-list.component';
 import { AdminProductAddCategoryComponent } from './admin-product-add-category/admin-product-add-category.component';
 import { AdminProductEditCategoryComponent } from './admin-product-edit-category/admin-product-edit-category.component';
+import { UserComponent } from '../user/user.component';
+import { USER_ROUTES } from '../user/user.routing';
+import { Auth2Guard } from '../user/guards/auth2.guard';
+import { UserDetailComponent } from '../user/user-detail/user-detail.component';
 
 
 
@@ -18,4 +22,6 @@ export const ADMIN_ROUTES: Routes = [
   {path: 'list', component: AdminProductListComponent},
   {path: 'add-category', component: AdminProductAddCategoryComponent},
   {path: 'edit-category', component: AdminProductEditCategoryComponent},
+  {path: 'users', component: UserComponent, children: USER_ROUTES, canActivate: [Auth2Guard]},
+  {path: 'detail/:id', component: UserDetailComponent},
 ];
