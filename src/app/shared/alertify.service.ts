31f497cd848d277d9ee3179d213018d1964a5a92
Bruno Staub <bruno.staub@gmail.com>
2018-11-30 11:34:40 +0100

    position alertify bottom right now

--- src/app/shared/alertify.service.ts
+++ src/app/shared/alertify.service.ts
@@ -31,7 +31,7 @@ alertify.defaults = {
     // auto-dismiss wait time (in seconds)
     delay: 5,
     // default position
-    position: 'top-right',
+    position: 'bottom-right',
     // adds a close button to notifier messages
     closeButton: false
   },
