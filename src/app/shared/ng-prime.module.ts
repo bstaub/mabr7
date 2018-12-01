2f498c15967e5a36453a55bda510d3e89c189d53
Bruno Staub <bruno.staub@gmail.com>
2018-12-01 17:20:29 +0100

    lazyloading checkout and products width core! widthout flyoutservice, sliderservice 2

--- src/app/shared/ng-prime.module.ts
+++ src/app/shared/ng-prime.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CheckboxModule, DropdownModule, EditorModule, FileUploadModule } from 'primeng/primeng';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RatingModule } from 'primeng/rating';
 
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from '../app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
