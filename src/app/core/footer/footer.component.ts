191f797813aefc08c51ccd883bf43ad3be4ba2ad
Bruno Staub <bruno.staub@gmail.com>
2018-12-08 10:42:55 +0100

    footer componente angepasst

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,8 +1,6 @@
-import { Component, isDevMode, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductCategory } from '../../models/product-category.model';
-import { ProductCategoryService } from '../../product/shared/product-category.service';
-import { SettingsService } from '../../shared/settings.service';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 
@@ -16,14 +14,14 @@ export class FooterComponent implements OnInit {
   categories$: Observable<ProductCategory[]>;
   url: string;
   productCategoryCollection: AngularFirestoreCollection<ProductCategory>;
-  categories$: Observable<ProductCategory[]>;
+  categoriesfooter$: Observable<ProductCategory[]>;
 
-  constructor(private productCategory: ProductCategoryService,
+  constructor(
               public afs: AngularFirestore
   ) {
 
     this.productCategoryCollection = this.afs.collection('categories', ref => ref.orderBy('name', 'asc'));
-    this.categories$ = this.productCategoryCollection.snapshotChanges().pipe(
+    this.categoriesfooter$ = this.productCategoryCollection.snapshotChanges().pipe(
       map(actions => actions.map(a => {
         const data = a.payload.doc.data() as ProductCategory;
         const key = a.payload.doc.id;
@@ -33,7 +31,6 @@ export class FooterComponent implements OnInit {
   }
 
   ngOnInit() {
-     // this.categories$ = this.productCategory.getCategories();
   }
 
 
