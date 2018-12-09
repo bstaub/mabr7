fed4b6840aba3525614d3116eea2a1807a64dccb
Martin Polak <martinpolak.mail@gmail.com>
2018-12-09 12:04:24 +0100

    changed logo font

--- src/app/core/home/home-slider-ngcarousel/home-slider-ngcarousel.component.ts
+++ src/app/core/home/home-slider-ngcarousel/home-slider-ngcarousel.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Slide } from '../../../models/Slide';
 import { SliderService } from '../../../shared/slider.service';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-home-slider-ngcarousel',
   templateUrl: './home-slider-ngcarousel.component.html',
-  styleUrls: ['./home-slider-ngcarousel.component.scss']
+  styleUrls: ['./home-slider-ngcarousel.component.scss'],
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
 export class HomeSliderNgcarouselComponent implements OnInit {
 
+
   sliderdata: Slide[];
 
-  constructor(private sliderService: SliderService) {
+  constructor(private sliderService: SliderService,
+              config: NgbCarouselConfig
+  ) {
+    // customize default values of carousels used by this component tree
+    config.interval = 0;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
 
   }
 
--- src/app/shared/slider.service.ts
+++ src/app/shared/slider.service.ts
@@ -22,25 +22,25 @@ export class SliderService {
 
     this.slider = [
       {
-        image: `assets/slider/slider1_guetzli.jpeg`,
+        image: 'assets/slider/slider1_guetzli.jpeg',
         title: 'Photoapparate',
         text: 'Schiesse die besten Photos mit unseren Digitalkameras',
         link: `${this.url}/produkte/list?category=Fotoapparate`,
       },
       {
-        image: `assets/slider/slider2_guetzli.jpeg`,
+        image: 'assets/slider/slider2_guetzli.jpeg',
         title: 'Gartenzubehör und Gartenartikel',
         text: 'Alles für den Garten',
         link: `${this.url}//produkte/list?category=Gartenzubeh%C3%B6r`,
       },
       {
-        image: `assets/slider/slide_sonnenbrillen.jpg`,
+        image: 'assets/slider/slide_sonnenbrillen.jpg',
         title: 'Sonnenbrillen',
         text: 'Die neusten Brillenmodelle passend zu den Jahrezeiten',
         link: `${this.url}//produkte/list?category=Sonnenbrillen`,
       },
       {
-        image: `assets/slider/laedeli_mydeer_opt.jpg`,
+        image: 'assets/slider/laedeli_mydeer_opt.jpg',
         title: 'Stoff & Kuscheltierli',
         text: 'Ein passendes Geschenk zu jeder Jahreszeit',
         link: `${this.url}/produkte/list?category=Maileg%20Tierchen`,
