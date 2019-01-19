e6e296c171060ff44a3122221ab804c9690a5477
Bruno Staub <bruno.staub@gmail.com>
2019-01-19 12:05:59 +0100

    local .env file added for security reason, api key removed from github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyChyTr5IeqbAUc44Rl8SCOOmHCLadziNB4',
-    authDomain: 'mabr7-shop.firebaseapp.com',
-    databaseURL: 'https://mabr7-shop.firebaseio.com',
-    projectId: 'mabr7-shop',
-    storageBucket: 'mabr7-shop.appspot.com',
-    messagingSenderId: '983183286327'
-  }
 };
