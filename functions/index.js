f0aa314771d237c2a2996c3e528af67478b67696
Bruno Staub <bruno.staub@gmail.com>
2019-01-19 14:44:08 +0100

    local .env file added for security reason, api key removed from github

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 const sendgridemail = require('@sendgrid/mail');
-const MY_SENDGRID_API_KEY = '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'
+const MY_SENDGRID_API_KEY = functions.config().sendgrid.key;
 sendgridemail.setApiKey(MY_SENDGRID_API_KEY);
 
 
@@ -22,7 +22,7 @@ exports.orderEmail = functions.firestore
           to: orderdata.customerBillingAddress.mail,
           bcc: 'mabrweb@gmail.com',
           from: 'mabrweb@gmail.com',
-          subject:  'Deine Bestellung bei mabr-web',
+          subject: 'Deine Bestellung bei mabr-web',
           templateId: 'd-0efa1b117040446485a43bdb1727d96b',
           substitutionWrappers: ['{{', '}}'],
           dynamicTemplateData: {
@@ -33,6 +33,6 @@ exports.orderEmail = functions.firestore
         console.log(msgbody);
         return sendgridemail.send(msgbody)
       })
-      .then(() => console.log('order mail sent success') )
-      .catch(err => console.log("shit "+ err) )
+      .then(() => console.log('order mail sent success'))
+      .catch(err => console.log("error " + err))
   });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: 'AIzaSyChyTr5IeqbAUc44Rl8SCOOmHCLadziNB4',
+    authDomain: 'mabr7-shop.firebaseapp.com',
+    databaseURL: 'https://mabr7-shop.firebaseio.com',
+    projectId: 'mabr7-shop',
+    storageBucket: 'mabr7-shop.appspot.com',
+    messagingSenderId: '983183286327'
+  }
 };
 
 /*
